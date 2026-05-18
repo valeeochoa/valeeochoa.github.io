a567021ae80dd7e7c2f7f3ab8532d36b84117289
valeeochoa <vochoaolguin@gmail.com>
2026-05-18 18:02:14 -0300

    cv cambios

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#ee2b5b",
+        "background-light": "#f8f6f6",
+        "background-dark": "#221015",
+      },
+      fontFamily: {
+        "display": ["Spline Sans", "sans-serif"]
+      },
+      borderRadius: { "DEFAULT": "0.5rem", "lg": "1rem", "xl": "1.5rem", "full": "9999px" },
+    },
+  },
+}
